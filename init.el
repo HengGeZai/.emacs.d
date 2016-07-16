73211f19bec21bb3365a7738fb0942f9692cfc96
HengGeZai <231111669@qq.com>
2016-07-16 08:27:46 +0800

    config2016-07-16

--- init.el
+++ init.el
@@ -6,3 +6,7 @@
 (org-babel-load-file (expand-file-name "init-functions.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init-packages.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init-keybindings.org" user-emacs-directory))
+
+;; 将customize修改的配置自动存放在customize.el中
+(setq custom-file (expand-file-name "lisp/customize.el" user-emacs-directory))
+(load-file custom-file)
