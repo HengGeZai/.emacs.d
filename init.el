74b6126e9be4d5f09d7127a94e5301e06cd4b5f9
HengGeZai <231111669@qq.com>
2016-08-05 20:08:36 +0800

    config Evil-mode iedit-mode hungry-mode r-autoyas

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq ts-init (current-time))
 (package-initialize)
 
 (require 'org-install)
@@ -7,6 +8,14 @@
 (org-babel-load-file (expand-file-name "init-packages.org" user-emacs-directory))
 (org-babel-load-file (expand-file-name "init-keybindings.org" user-emacs-directory))
 
+                                        ; elc does not improve the startup time-add
+;; (load-file "~/.emacs.d/init-better-defaults.elc")
+;; (load-file "~/.emacs.d/init-packages.elc")
+;; (load-file "~/.emacs.d/init-keybindings.elc")
+
 ;; 将customize修改的配置自动存放在customize.el中
 (setq custom-file (expand-file-name "lisp/customize.el" user-emacs-directory))
 (load-file custom-file)
+
+(message "Finish startup in %.2f seconds"
+         (float-time (time-since ts-init)))
