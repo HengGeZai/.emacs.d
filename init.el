3262e3a460ffdbf6ea66d794fbb4441164347b42
HengGeZai <231111669@qq.com>
2016-08-25 15:32:29 +0800

    do not display time while starting emacs

--- init.el
+++ init.el
@@ -18,8 +18,5 @@
 (setq custom-file (expand-file-name "lisp/customize.el" user-emacs-directory))
 (load-file custom-file)
 
-(message "Finish startup in %.2f seconds"
-         (float-time (time-since ts-init)))
-
 ;; 设置scratch默认显示的消息
 (setq initial-scratch-message (concat ";; This buffer is for notes you don't want to save, and for Lisp evaluation.\n;; If you want to create a file, visit that file with C-x C-f\n;; If you want to config emacs, please type F2\n;; If you want to Agenda, please type C-c a\n;; Finish startup in " (number-to-string (float-time (time-since ts-init))) " seconds\n\n"))
