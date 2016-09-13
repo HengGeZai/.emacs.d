7e08011ce99cf8e9a57486be7d5c443f9041a167
HengGeZai <231111669@qq.com>
2016-09-13 12:16:40 +0800

    org-zotxt-default-search-method :title-creator-year

--- lisp/customize.el
+++ lisp/customize.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-zotxt-default-search-method :title-creator-year)
  '(pyim-page-length 9)
  '(pyim-punctuation-dict
    (quote
